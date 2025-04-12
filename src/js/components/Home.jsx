ee6cd58ccee217959d0e70db640c3bb1e0186160
naiomi25 <naiomidiaz.her@gmail.com>
2025-04-12 09:49:05 +0000

    lista de tareas terminada

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [input, setInput] = useState('')
+	const [tareas, setTareas] = useState([])
+
+
+
+	const ListaTareas = (e) => {
+		if (e.key === 'Enter') {
+			setTareas([...tareas, input])
+			setInput('')
+		}
+
+	}
+	const handleremove = (tarea) => {
+
+		const Tareasactualizadas = tareas.filter(e => e !== tarea)
+		setTareas(Tareasactualizadas)
+		if (Tareasactualizadas.length === 0) {
+			setInput('No hay mas tareas, introduzca una')
+		}
+
+	}
+	const handleCleanMessage = () => {
+		if (tareas.length === 0) {
+			setInput('')
+		}
+	}
+
+	return (
+		<div className="ContenedorTareas ">
+			<h1>LISTA DE TAREAS</h1>
+
+			<input className="input" type="text" value={input} onChange={e => setInput(e.target.value)} onKeyDown={ListaTareas} onMouseEnter={handleCleanMessage} placeholder="Introduce tarea" />
+
+			<ul className="ul" >
+				{tareas.map((tarea, index) => <div className="listaOrdenada" key={index}>
+					<li className="lista">{tarea}</li>
+					<button className="boton-eliminar" onClick={() => (handleremove(tarea))}>❌</button>
+
+				</div>)}
+			</ul>
+
+
 		</div>
+
 	);
 };
 
